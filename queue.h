773d5a1d54113a3907edfb1a199face6d8f51d34
Fabio Torres <fabio.torres@utec.edu.pe>
2021-09-12 16:30:36 -0300

    Add inheritance functions from circularArray

--- queue.h
+++ queue.h
@@ -2,5 +2,51 @@
 
 template <typename T>
 class QueueArray : public CircularArray<T> { 
-    
+    private:
+        int* array;
+        int capacity;
+        int front, back;
+    public:
+    QueueArray();
+    void enqueue(int data);
+    int dequeue();
+    void display();
 };
+
+template <class T>
+QueueArray<T>::QueueArray()
+{
+    QueueArray(0);
+}
+
+template <class T>
+QueueArray<T>::QueueArray(int _capacity)
+{
+    this->capacity = _capacity;
+    this->front = this->back = -1;
+    this->array = new T[_capacity];
+}
+
+template <class T>
+void QueueArray<T>::enqueue(int data)
+{
+    if(is_full()) resize();
+    reat = next(rear);
+    array[rear] = data;
+}
+
+template <class T>
+void QueueArray<T>::dequeue(int data)
+{
+    if(is_empty()) throw("is empty");
+    front = next(front);
+}
+
+template <class T>
+void QueueArray<T>::display()
+{    
+    for (int i = 0; i < size(); i++)
+        cout<<(*this)[i]<<",";        
+    cout<<endl;
+}
+
--- stack.h
+++ stack.h
@@ -2,5 +2,46 @@
 
 template <typename T>
 class StackArray : public CircularArray<T> { 
-    
+    private:
+        int* array;
+        int capacity;
+        int size;
+    public:
+        StackArray();
+        void push(T data);
+        int pop();
+        void display();
 };
+
+template <class T>
+StackArray<T>::StackArray()
+{
+    StackArray(0);
+}
+
+template <class T>
+StackArray<T>::StackArray(int _capacity)
+{
+    this->capacity = _capacity;
+    this->size = -1;
+    this->array = new T[_capacity];
+}
+
+template <class T>
+void StackArray<T>::push(T data)
+{
+    push_back(data);
+}
+
+template <class T>
+int StackArray<T>::pop(){
+    return pop_back();
+}
+
+template <class T>
+void StackArray<T>::display()
+{    
+    for (int i = 0; i < size(); i++)
+        cout<<(*this)[i]<<",";        
+    cout<<endl;
+}
